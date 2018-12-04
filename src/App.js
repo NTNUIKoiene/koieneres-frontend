c1a86703be5ea39f3b7a947ac49379f41dd1dcca
Espen Meidell <espen.meidell@gmail.com>
2018-12-04 14:02:06 +0100

    change routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 import Login from "./components/Login";
 import Booking from "./components/Booking";
 import Reservations from "./components/Reservations";
@@ -9,9 +14,9 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <Route exact path="/" component={Booking} />
+          <Route exact path="/" component={Login} />
           <Route path="/reservations" component={Reservations} />
-          <Route exact path="/login" component={Login} />
+          <Route path="/booking" component={Booking} />
         </div>
       </Router>
     );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,22 +3,25 @@ import styles from "./Login.module.css";
 import { TextField, PrimaryButton } from "office-ui-fabric-react";
 import { ColorClassNames, FontClassNames } from "@uifabric/styling";
 
-const Login = props => (
-  <div className={styles.loginscreen}>
-    <div className={styles.backgroundimage} />
-    <div className={styles.container}>
-      <h1
-        className={[ColorClassNames.themePrimary, FontClassNames.xLarge].join(
-          " "
-        )}
-      >
-        Velkommen til KoieneRes
-      </h1>
-      <TextField label="Brukernavn" />
-      <TextField type="password" label="Passord" />
-      <PrimaryButton className={styles.button}>Logg inn</PrimaryButton>
+const Login = props => {
+  console.log(props);
+  return (
+    <div className={styles.loginscreen}>
+      <div className={styles.backgroundimage} />
+      <div className={styles.container}>
+        <h1
+          className={[ColorClassNames.themePrimary, FontClassNames.xLarge].join(
+            " "
+          )}
+        >
+          Velkommen til KoieneRes
+        </h1>
+        <TextField label="Brukernavn" />
+        <TextField type="password" label="Passord" />
+        <PrimaryButton className={styles.button}>Logg inn</PrimaryButton>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Login;
