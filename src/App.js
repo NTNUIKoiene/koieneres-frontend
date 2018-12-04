3e66a09ee324cc88b0c3b84332c89edb036a8c95
Espen Meidell <espen.meidell@gmail.com>
2018-12-04 14:25:37 +0100

    Remove warnings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Booking from "./components/Booking";
 import Reservations from "./components/Reservations";
