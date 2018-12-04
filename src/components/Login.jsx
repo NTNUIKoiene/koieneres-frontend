e9b1dd564c77756c7fb7444198cad5b37a3c486f
Espen Meidell <espen.meidell@gmail.com>
2018-12-04 13:53:39 +0100

    Create theme, loginscreen

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import styles from "./Login.module.css";
+import { TextField, PrimaryButton } from "office-ui-fabric-react";
+import { ColorClassNames, FontClassNames } from "@uifabric/styling";
 
-const Login = props => <div>Login</div>;
+const Login = props => (
+  <div className={styles.loginscreen}>
+    <div className={styles.backgroundimage} />
+    <div className={styles.container}>
+      <h1
+        className={[ColorClassNames.themePrimary, FontClassNames.xLarge].join(
+          " "
+        )}
+      >
+        Velkommen til KoieneRes
+      </h1>
+      <TextField label="Brukernavn" />
+      <TextField type="password" label="Passord" />
+      <PrimaryButton className={styles.button}>Logg inn</PrimaryButton>
+    </div>
+  </div>
+);
 
 export default Login;
