881c283fffccf2ef00dc91e9533d896c225f7a5a
Espen Meidell <espen.meidell@gmail.com>
2018-12-04 13:03:03 +0100

    Add react router and some basic paths

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
+import Booking from "./components/Booking";
+import Reservations from "./components/Reservations";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Booking} />
+          <Route path="/reservations" component={Reservations} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
