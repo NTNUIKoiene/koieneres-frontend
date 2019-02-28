4bbc96f04ac75ca5152515556545e3c7e8cc4742
Espen Meidell <espen.meidell@gmail.com>
2019-02-28 15:23:29 +0100

    Can add closings

--- src/api.js
+++ src/api.js
@@ -9,4 +9,25 @@ const fetchAPIData = async path => {
   })).json();
 };
 
-export { fetchAPIData };
+const deleteAPIData = async path => {
+  return await fetch(`${BASE_URL}${path}`, {
+    headers: {
+      Authorization: `JWT ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json"
+    },
+    method: "DELETE"
+  });
+};
+
+const postAPIData = async (path, payload) => {
+  return await (await fetch(`${BASE_URL}${path}`, {
+    headers: {
+      Authorization: `JWT ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify(payload)
+  })).json();
+};
+
+export { fetchAPIData, deleteAPIData, postAPIData };
--- src/components/Closing.jsx
+++ src/components/Closing.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import nb from "date-fns/locale/nb";
+import { addDays, format } from "date-fns";
+import { datePickerStrings } from "../utils/DatePickerStrings";
+
 import { useState, useEffect } from "react";
 import Header from "./Header";
 import styles from "./Closing.module.css";
-import { fetchAPIData } from "../api";
-import { DefaultButton } from "office-ui-fabric-react";
+import { fetchAPIData, deleteAPIData, postAPIData } from "../api";
+import {
+  DefaultButton,
+  ComboBox,
+  Label,
+  DatePicker,
+  DayOfWeek,
+  PrimaryButton
+} from "office-ui-fabric-react";
 
 const Closing = props => {
   const [cabins, setCabins] = useState([]);
   const [existingClosings, setExistingClosings] = useState([]);
+  const [fromDate, setFromDate] = useState(new Date());
+  const [toDate, setToDate] = useState(addDays(new Date(), 1));
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedCabin, setSelectedCabin] = useState({
+    key: 0,
+    text: "Velg koie"
+  });
 
   const fetchData = async () => {
     const cabinData = await fetchAPIData("/api/cabin/");
     const existingClosingsData = await fetchAPIData("/api/cabin-closings/");
 
-    setCabins(cabinData.results);
+    setCabins(
+      cabinData.results.map(c => {
+        return { key: c.id, text: c.name };
+      })
+    );
     setExistingClosings(existingClosingsData.results);
   };
 
@@ -22,20 +43,82 @@ const Closing = props => {
     fetchData();
   }, []);
 
+  const deleteClosing = async id => {
+    setIsLoading(true);
+    await deleteAPIData(`/api/cabin-closings/${id}/`);
+    await fetchData();
+    setIsLoading(false);
+  };
+
+  const addClosing = async () => {
+    setIsLoading(true);
+    const payload = {
+      cabin: selectedCabin.key,
+      fromDate: format(fromDate, "YYYY-MM-DD"),
+      toDate: format(toDate, "YYYY-MM-DD")
+    };
+    await postAPIData("/api/cabin-closings/", payload);
+    await fetchData();
+    setIsLoading(false);
+  };
+
   return (
     <div>
       <Header currentPage={props.location.pathname} />
+      <div className={styles.addContainer}>
+        <br />
+        <ComboBox
+          autoComplete="on"
+          options={cabins}
+          placeholder="Select or type an option"
+          text={selectedCabin.text}
+          onChange={(_, o) => setSelectedCabin(o)}
+        />
+        <Label htmlFor="fromDate">fra</Label>
+        <DatePicker
+          id="fromDate"
+          value={fromDate}
+          onSelectDate={d => setFromDate(d)}
+          strings={datePickerStrings}
+          firstDayOfWeek={DayOfWeek.Monday}
+          formatDate={d => format(d, "dddd D MMM YYYY", { locale: nb })}
+          minDate={new Date()}
+        />
+        <Label htmlFor="toDate">til</Label>
+        <DatePicker
+          id="toDate"
+          value={toDate}
+          onSelectDate={d => setToDate(d)}
+          minDate={fromDate}
+          string={datePickerStrings}
+          firstDayOfWeek={DayOfWeek.Monday}
+          formatDate={d => format(d, "dddd D MMM YYYY", { locale: nb })}
+        />
+        <PrimaryButton
+          text="Lagre"
+          onClick={addClosing}
+          iconProps={
+            isLoading ? { iconName: "Hourglass" } : { iconName: "Save" }
+          }
+          disabled={selectedCabin.key === 0 || isLoading}
+        />
+      </div>
       <div className={styles.dataContainer}>
         <h2>Eksisterende Stenginger:</h2>
         {existingClosings.map((c, k) => (
-          <ExistingClosing key={k} closing={c} />
+          <ExistingClosing
+            key={k}
+            closing={c}
+            deleteClick={() => deleteClosing(c.id)}
+            isLoading={isLoading}
+          />
         ))}
       </div>
     </div>
   );
 };
 
-const ExistingClosing = ({ closing }) => {
+const ExistingClosing = ({ closing, deleteClick, isLoading }) => {
   return (
     <div className={styles.card}>
       <h3>{closing.cabin.name}</h3>
@@ -52,7 +135,14 @@ const ExistingClosing = ({ closing }) => {
         <span className={styles.value}>{closing.comment}</span>
       </div>
       <hr />
-      <DefaultButton iconProps={{ iconName: "Delete" }} text="Slett" />
+      <DefaultButton
+        text="Slett"
+        onClick={deleteClick}
+        disabled={isLoading}
+        iconProps={
+          isLoading ? { iconName: "Hourglass" } : { iconName: "Delete" }
+        }
+      />
     </div>
   );
 };
