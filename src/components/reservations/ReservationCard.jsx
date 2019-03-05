af63a09f984d327a0fe2ac3e7e0a44b771464993
Markus Andresen <markus.andresen94@gmail.com>
2019-03-05 11:28:49 +0100

    Edit functionality implemented

--- src/components/reservations/ReservationCard.jsx
+++ src/components/reservations/ReservationCard.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import styles from "./ReservationCard.module.css";
 import { format } from "date-fns";
-import { DefaultButton, PrimaryButton } from "office-ui-fabric-react";
+import {
+  DefaultButton,
+  PrimaryButton,
+  Modal,
+  TextField,
+  Checkbox
+} from "office-ui-fabric-react";
 import { patchAPIData } from "../../api";
 import { BASE_URL } from "../../config";
 import { useState } from "react";
@@ -25,6 +31,17 @@ const ReservationCard = ({ reservation, reload }) => {
   const dates = reservation.reservationItems.map(r => r.date).join(", ");
 
   const [receiptButtonDisabled, setReceiptButtonDisabled] = useState(false);
+  const [markAsPaidDisabled, setMarkAsPaidDisabled] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+
+  const [nameState, setNameState] = useState(name);
+  const [membershipNumberState, setMembershipNumberState] = useState(
+    membershipNumber
+  );
+  const [phoneState, setPhoneState] = useState(phone);
+  const [emailState, setEmailState] = useState(email);
+  const [isPaidState, setIsPaidState] = useState(isPaid);
+  const [shouldPayState, setShouldPayState] = useState(shouldPay);
 
   const onReceiptClick = async () => {
     setReceiptButtonDisabled(true);
@@ -41,97 +58,168 @@ const ReservationCard = ({ reservation, reload }) => {
     setReceiptButtonDisabled(false);
   };
 
-  const [markAsPaidDisabled, setMarkAsPaidDisabled] = useState(false);
-  const [isPaidState, setIsPaidState] = useState(isPaid);
+  const onEditConfirmClick = async () => {
+    await patchAPIData("/api/reservationdata/" + id, {
+      name: nameState,
+      membershipNumber: membershipNumberState,
+      phone: phoneState,
+      email: emailState,
+      shouldPay: shouldPayState,
+      is_paid: isPaidState
+    });
+    setEditModalOpen(false);
+    reload();
+  };
 
   const onMarkAsPaidClick = async e => {
     setMarkAsPaidDisabled(true);
     await patchAPIData("/api/reservationdata/" + id, {
-      is_paid: true
+      isPaid: true
     });
-    setIsPaidState(true);
+    reload();
   };
 
   return (
-    <div className={styles.card}>
-      <h3>{`#${id}: ${cabin}, ${firstDate} (${numberOfNights} ${
-        numberOfNights === 1 ? "natt" : "netter"
-      })`}</h3>
-      <section className={styles.info}>
-        <div>
-          <span className={styles.label}>Reservasjonstidspunkt:</span>
-          <span className={styles.value}>
-            {format(new Date(createdAt), "YYYY-MM-DD HH:mm")}
-          </span>
-        </div>
-        <hr />
-        <div>
-          <span className={styles.label}>Navn:</span>
-          <span className={styles.value}>{name}</span>
-        </div>
-        <div>
-          <span className={styles.label}>Medlemsnummer:</span>
-          <span className={styles.value}>{membershipNumber}</span>
-        </div>
-        <div>
-          <span className={styles.label}>Epost:</span>
-          <span className={styles.value}>{email}</span>
-        </div>
-        <div>
-          <span className={styles.label}>Telefon:</span>
-          <span className={styles.value}>{phone}</span>
-        </div>
-        <hr />
-        <div>
-          <span className={styles.label}>Datoer:</span>
-          <span className={styles.value}>{dates}</span>
-        </div>
-        <div>
-          <span className={styles.label}>Totalpris:</span>
-          <span className={styles.value}>{totalPrice} NOK</span>
-        </div>
-        <div>
-          <span className={styles.label}>Betalingsstatus:</span>
-          {shouldPay ? (
-            isPaidState ? (
-              <span className={styles.value}>Betalt</span>
-            ) : (
-              <span className={styles.unpaid}>Ubeltalt</span>
-            )
-          ) : (
-            <span className={styles.value}>Gratis</span>
-          )}
-        </div>
-        <hr />
-        <div>
-          <DefaultButton
-            iconProps={
-              receiptButtonDisabled
-                ? { iconName: "Hourglass" }
-                : { iconName: "PDF" }
-            }
-            ariaLabel="Receipt"
-            onClick={onReceiptClick}
-            disabled={receiptButtonDisabled}
-          >
-            Kvittering
-          </DefaultButton>
-          {!isPaidState && shouldPay && (
-            <PrimaryButton
-              iconProps={
-                markAsPaidDisabled
-                  ? { iconName: "Hourglass" }
-                  : { iconName: "Money" }
-              }
-              ariaLabel="Mark as paid"
-              text="Marker som betalt"
-              className={styles.payBtn}
-              disabled={markAsPaidDisabled}
-              onClick={onMarkAsPaidClick}
+    <div>
+      <Modal isOpen={editModalOpen} onDismiss={() => setEditModalOpen(false)}>
+        <div className={styles.editWrapper}>
+          <h1>Rediger reservasjon</h1>
+          <TextField
+            label="Navn"
+            value={nameState}
+            onChange={e => setNameState(e.target.value)}
+          />
+          <TextField
+            label="Medlemsnummer"
+            value={membershipNumberState}
+            onChange={e => setMembershipNumberState(e.target.value)}
+          />
+          <TextField
+            label="Telefon"
+            value={phoneState}
+            onChange={e => setPhoneState(e.target.value)}
+          />
+          <TextField
+            label="Epost"
+            value={emailState}
+            onChange={e => setEmailState(e.target.value)}
+          />
+          <Checkbox
+            className={styles.editPayBtn}
+            checked={shouldPayState}
+            label="Koia skal betales for"
+            onChange={e => setShouldPayState(!shouldPayState)}
+          />
+          {shouldPayState && (
+            <Checkbox
+              className={styles.editPayBtn}
+              checked={isPaidState}
+              label="Koia er betalt"
+              onChange={e => setIsPaidState(!isPaidState)}
             />
           )}
+          <PrimaryButton
+            className={styles.confirmEditBtn}
+            onClick={onEditConfirmClick}
+          >
+            Bekreft endring
+          </PrimaryButton>
+          <DefaultButton onClick={() => setEditModalOpen(false)}>
+            Avbryt
+          </DefaultButton>
         </div>
-      </section>
+      </Modal>
+
+      <div className={styles.card}>
+        <h3>{`#${id}: ${cabin}, ${firstDate} (${numberOfNights} ${
+          numberOfNights === 1 ? "natt" : "netter"
+        })`}</h3>
+        <section className={styles.info}>
+          <div>
+            <span className={styles.label}>Reservasjonstidspunkt:</span>
+            <span className={styles.value}>
+              {format(new Date(createdAt), "YYYY-MM-DD HH:mm")}
+            </span>
+          </div>
+          <hr />
+          <div>
+            <span className={styles.label}>Navn:</span>
+            <span className={styles.value}>{name}</span>
+          </div>
+          <div>
+            <span className={styles.label}>Medlemsnummer:</span>
+            <span className={styles.value}>{membershipNumber}</span>
+          </div>
+          <div>
+            <span className={styles.label}>Epost:</span>
+            <span className={styles.value}>{email}</span>
+          </div>
+          <div>
+            <span className={styles.label}>Telefon:</span>
+            <span className={styles.value}>{phone}</span>
+          </div>
+          <hr />
+          <div>
+            <span className={styles.label}>Datoer:</span>
+            <span className={styles.value}>{dates}</span>
+          </div>
+          <div>
+            <span className={styles.label}>Totalpris:</span>
+            <span className={styles.value}>{totalPrice} NOK</span>
+          </div>
+          <div>
+            <span className={styles.label}>Betalingsstatus:</span>
+            {shouldPay ? (
+              isPaid ? (
+                <span className={styles.value}>Betalt</span>
+              ) : (
+                <span className={styles.unpaid}>Ubeltalt</span>
+              )
+            ) : (
+              <span className={styles.value}>Gratis</span>
+            )}
+          </div>
+          <hr />
+          <div>
+            <DefaultButton
+              iconProps={{ iconName: "Edit" }}
+              ariaLabel="Edit"
+              className={styles.editBtn}
+              onClick={() => {
+                setEditModalOpen(true);
+              }}
+            >
+              Rediger
+            </DefaultButton>
+            <DefaultButton
+              iconProps={
+                receiptButtonDisabled
+                  ? { iconName: "Hourglass" }
+                  : { iconName: "PDF" }
+              }
+              ariaLabel="Receipt"
+              onClick={onReceiptClick}
+              disabled={receiptButtonDisabled}
+            >
+              Kvittering
+            </DefaultButton>
+            {!isPaid && shouldPay && (
+              <PrimaryButton
+                iconProps={
+                  markAsPaidDisabled
+                    ? { iconName: "Hourglass" }
+                    : { iconName: "Money" }
+                }
+                ariaLabel="Mark as paid"
+                text="Marker som betalt"
+                className={styles.payBtn}
+                disabled={markAsPaidDisabled}
+                onClick={onMarkAsPaidClick}
+              />
+            )}
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
