9603dd480fcd9aae75b52dcb09a2138e6b30c2c9
Markus Andresen <markus.andresen94@gmail.com>
2019-03-28 21:16:57 +0100

    useMiddleware a hook that wraps useReducer's dispatch method with any redux middleware

---
+++ src/hooks/useMiddleware.js
@@ -0,0 +1,29 @@
+import { useReducer } from "react";
+
+const compose = (...funcs) => {
+  return arg => funcs.reduceRight((composed, fn) => fn(composed), arg);
+};
+
+const applyMiddleware = middlewares => {
+  return createStore => (...args) => {
+    const store = createStore(...args);
+    const chain = middlewares.map(middleware => middleware(store));
+    let dispatch = store.dispatch;
+    dispatch = compose(...chain)(store.dispatch);
+    return { ...store, dispatch };
+  };
+};
+
+const useMiddleware = (reducer, initalState, ...middlewares) => {
+  if (typeof reducer !== "function") {
+    throw new Error("Expected the reducer to be a function.");
+  }
+  const [state, dispatch] = useReducer(reducer, initalState);
+  const createStore = () => ({ dispatch, getState: () => state });
+  const { getState, dispatch: wrappedDispatch } = applyMiddleware(middlewares)(
+    createStore
+  )();
+  return [getState(), wrappedDispatch];
+};
+
+export default useMiddleware;
