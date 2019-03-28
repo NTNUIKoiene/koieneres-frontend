7174cff3f39868971020a525233d74e52a68dd9f
Markus Andresen <markus.andresen94@gmail.com>
2019-03-28 12:55:37 +0100

    useAbortableRequest hook, WIP

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -24,4 +24,45 @@ const useUserConfig = () => {
   return userConfig;
 };
 
-export { useUserConfig };
+const useAbortableRequest = (
+  initalState,
+  method,
+  url,
+  handleSuccess,
+  handleError,
+  deps = []
+) => {
+  // TODO: Support dependency array (deps)
+  const [state, setState] = useState(initalState);
+  const source = axios.CancelToken.source();
+  let didCancel = false;
+
+  useEffect(() => {
+    const request = async () => {
+      try {
+        const response = await axios({
+          method,
+          url,
+          data: {
+            cancelToken: source.token
+          }
+        });
+        setState(handleSuccess(response));
+      } catch (error) {
+        console.log(error);
+        if (!didCancel) {
+          handleError(error);
+        }
+      }
+    };
+    request();
+    return () => {
+      didCancel = true;
+      source.cancel();
+    };
+  }, deps);
+
+  return state;
+};
+
+export { useUserConfig, useAbortableRequest };
