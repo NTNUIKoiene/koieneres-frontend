a3f0ebc345b4cb00a19ee8a227842fdcbac02aa8
Markus Andresen <markus.andresen94@gmail.com>
2019-03-29 16:03:05 +0100

    Hook now functions as expected

--- src/hooks/useAbortableRequest.js
+++ src/hooks/useAbortableRequest.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { BASE_URL } from "../config";
 
@@ -8,36 +8,43 @@ export default function useAbortableRequest(
   handleError,
   method = "get"
 ) {
-  const source = axios.CancelToken.source();
-  let didCancel = false;
-
-  const request = async () => {
-    try {
-      const response = await axios({
-        method: method.toLowerCase(),
-        url: BASE_URL + url,
-        data: {
-          cancelToken: source.token
-        }
-      });
-      if (!didCancel) {
-        handleSuccess(response);
-      }
-    } catch (error) {
-      if (!didCancel) {
-        handleError(error);
-      }
-    }
-    return didCancel;
-  };
+  const [effectCount, setEffectCount] = useState(0);
+  const isMounted = useRef(true);
 
   useEffect(() => {
+    const source = axios.CancelToken.source();
+    let didCancel = false;
+
+    const request = async () => {
+      try {
+        const response = await axios({
+          method: method.toLowerCase(),
+          url: BASE_URL + url,
+          data: {
+            cancelToken: source.token
+          }
+        });
+        if (!didCancel) {
+          handleSuccess(response);
+        }
+      } catch (error) {
+        if (!didCancel) {
+          handleError(error);
+        }
+      }
+      return didCancel;
+    };
     request();
     return () => {
       didCancel = true;
       source.cancel();
+      isMounted.current = false;
     };
-  }, []);
+  }, [effectCount]);
 
-  return request;
+  return () => {
+    if (isMounted.current) {
+      setEffectCount(prevCount => prevCount + 1);
+    }
+  };
 }
