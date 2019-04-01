f37f9cecd3b575223c0bd9b9d0694ae18d775352
Markus Andresen <markus.andresen94@gmail.com>
2019-04-01 18:00:40 +0200

    Resolved bug and removed unnecessary code

--- src/hooks/useMiddleware.js
+++ src/hooks/useMiddleware.js
@@ -1,33 +1,25 @@
-import { useReducer } from "react";
+import { useState } from "react";
 
 const compose = (...funcs) => {
   return arg => funcs.reduceRight((composed, fn) => fn(composed), arg);
-};
-
-const applyMiddleware = middlewares => {
-  return createStore => (...args) => {
-    const store = createStore(...args);
-    const chain = middlewares.map(middleware =>
-      middleware({
-        getState: store.getState,
-        dispatch: action => dispatch(action)
-      })
-    );
-    const dispatch = compose(...chain)(store.dispatch);
-    return { ...store, dispatch };
-  };
 };
 
 const useMiddleware = (reducer, initalState, ...middlewares) => {
   if (typeof reducer !== "function") {
     throw new Error("Expected the reducer to be a function.");
   }
-  const [state, dispatch] = useReducer(reducer, initalState);
-  const createStore = () => ({ dispatch, getState: () => state });
-  const { getState, dispatch: wrappedDispatch } = applyMiddleware(middlewares)(
-    createStore
-  )();
-  return [getState(), wrappedDispatch];
+  const [state, setState] = useState(initalState);
+
+  const dispatch = action => {
+    const nextState = reducer(state, action);
+    setState(nextState);
+    return action;
+  };
+  const store = { getState: () => state, dispatch };
+  const chain = middlewares.map(middleware => middleware(store));
+
+  const wrappedDispatch = compose(...chain)(store.dispatch);
+  return [state, wrappedDispatch];
 };
 
 export default useMiddleware;
